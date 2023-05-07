e6b063adb9198e0687a992a9eecfac6ecd4816f6
onion <kosukekito38@gmail.com>
2023-05-07 15:54:03 +0900

    feat: use slack/bolt environment variables in App Runner container

--- infrastructure/lib/app-runner.ts
+++ infrastructure/lib/app-runner.ts
@@ -1,5 +1,6 @@
-import * as cdk from 'aws-cdk-lib'
-import { aws_iam as iam, aws_ecr as ecr } from 'aws-cdk-lib'
+import * as cdk from 'aws-cdk-lib';
+import { aws_iam as iam, aws_ecr as ecr } from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
 
@@ -14,16 +15,26 @@ export class AppRunner extends Construct {
 
     const { repository } = props
 
+    // get pre-provisioned secrets from AWS SecretsManager
+    const secretSlackBotToken = secretsmanager.Secret.fromSecretNameV2(scope, 'SecretSlackBotToken', 'Onition-Backend/SLACK_BOT_TOKEN');
+    const secretSlackSigningSecret = secretsmanager.Secret.fromSecretNameV2(scope, 'SecretSlackSigningSecret', 'Onition-Backend/SLACK_SIGNING_SECRET');
+
     // Apprunner
     const appRunnerService = new apprunner.Service(scope, 'OnitionBackendAppRunnerService', {
       source: apprunner.Source.fromEcr({
         imageConfiguration: {
           port: 3000,
+          environmentSecrets: {
+            SLACK_BOT_TOKEN: apprunner.Secret.fromSecretsManager(secretSlackBotToken),
+            SLACK_SIGNING_SECRET: apprunner.Secret.fromSecretsManager(secretSlackSigningSecret)
+          }
         },
-        repository,
-        tag: 'latest',
+        repository: repository,
+        tagOrDigest: 'latest',
       }),
-    })
+      autoDeploymentsEnabled: true
+    });
+
     new cdk.CfnOutput(scope, 'AppUrl', {
       value: appRunnerService.serviceUrl
     });
