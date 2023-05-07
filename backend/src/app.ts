8f1ab804fdebfee8f899de13fe53387d47262b72
onion <kosukekito38@gmail.com>
2023-05-07 15:52:59 +0900

    feat: replying bot

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,49 @@
 import * as bolt from '@slack/bolt'
 
-const { App } = require(`@slack/bolt`)
-
-const app = new App({
+const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel: bolt.LogLevel.INFO,
 });
 
 app.message(':wave:', async ({ message, say }) => {
   await say(`Hello, <@${(message as bolt.GenericMessageEvent).user}>`);
+});
+
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${(message as bolt.GenericMessageEvent).user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${(message as bolt.GenericMessageEvent).user}>!`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+// Listens to incoming messages that contain "goodbye"
+app.message('goodbye', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`See ya later, <@${(message as bolt.GenericMessageEvent).user}> :wave:`);
 });
 
 (async () => {
